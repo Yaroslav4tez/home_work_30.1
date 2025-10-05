fb9b9e45147b9a3eb44ac722b3f0609cde415df7
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-10-05 19:00:15 +0200

    more code

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
+
